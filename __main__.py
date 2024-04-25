d1bb8cc45f3e27adca1883fe90dbb735cf7082e3
Johnaif Abdul <johnaif.abdul@g.msuiit.edu.ph>
2024-04-25 16:32:45 +0800

    Tried out something

--- __main__.py
+++ __main__.py
@@ -1,13 +1,12 @@
 from mpi4py import MPI
 import numpy as np
-import random
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
+size = comm.size
 
 def matrix_randomizer(n):
     matrix = np.random.randint(0, 11, size=(n, n))
-    print(f"matrix: {matrix}")
     return matrix
 
 def split(matrix):
@@ -39,63 +38,47 @@ def mini_strassen(mat1, mat2):
 
     return c
 
-def strassen(m1, m2, rank):
+def strassen(m1, m2):
     if len(m1) == 1:
         return m1 * m2
 
     a, b, c, d = split(m1)
     e, f, g, h = split(m2)
 
-    p1 = p2 = p3 = p4 = p5 = p6 = p7 = []
+    local_result = mini_strassen(a, f - h)
 
-    if (rank==0):
-        p1 = comm.recv(source=1,tag=1)
-        p2 = comm.recv(source=2,tag=2)
-        p3 = comm.recv(source=3,tag=3)
-        p4 = comm.recv(source=4,tag=4)
-        p5 = comm.recv(source=5,tag=5)
-        p6 = comm.recv(source=6,tag=6)
-        p7 = comm.recv(source=7,tag=7)
-
-    if (rank==1):
-        p1 = mini_strassen(a, f - h)
-        comm.send(p1,0,rank)
-    if (rank==2):
-        p2 = mini_strassen(a + b, h)
-        comm.send(p2,0,rank)
-    if (rank==3):
-        p3 = mini_strassen(c + d, e)
-        comm.send(p3,0,rank)
-    if (rank==4):
-        p4 = mini_strassen(d, g - e)
-        comm.send(p4,0,rank)
-    if (rank==5):
-        p5 = mini_strassen(a + d, e + h)
-        comm.send(p5,0,rank)
-    if (rank==6):
-        p6 = mini_strassen(b - d, g + h)
-        comm.send(p6,0,rank)
-    if (rank==7):
-        p7 = mini_strassen(a - c, e + f)
-        comm.send(p7,0,rank)
-
+    for i in range(1, size):
+        if rank == i:
+            comm.send(mini_strassen(a + b, h), dest=0, tag=1)
+            comm.send(mini_strassen(c + d, e), dest=0, tag=2)
+            comm.send(mini_strassen(d, g - e), dest=0, tag=3)
+            comm.send(mini_strassen(a + d, e + h), dest=0, tag=4)
+            comm.send(mini_strassen(b - d, g + h), dest=0, tag=5)
+            comm.send(mini_strassen(a - c, e + f), dest=0, tag=6)
+    
     comm.barrier()
 
-    if (rank==0):
-        c11 = p5 + p4 - p2 + p6
-        c12 = p1 + p2
-        c21 = p3 + p4
-        c22 = p1 + p5 - p3 - p7
+    if rank == 0:
+        p2 = comm.recv(source=1, tag=1)
+        p3 = comm.recv(source=2, tag=2)
+        p4 = comm.recv(source=3, tag=3)
+        p5 = comm.recv(source=4, tag=4)
+        p6 = comm.recv(source=5, tag=5)
+        p7 = comm.recv(source=6, tag=6)
+
+        c11 = local_result + p5 + p4 - p2 + p6 
+        c12 = local_result + p1 + p2         
+        c21 = local_result + p3 + p4         
+        c22 = local_result + p1 + p5 - p3 - p7
 
         c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22)))) 
         return c
 
-
 if __name__ == "__main__":
     n = 0
     m1 = m2 = []
-    product = []
-    if (rank == 0):
+
+    if rank == 0:
         try:
             print("Size for the (n x n) matrix? ")
             n = int(input())
@@ -112,10 +95,7 @@ if __name__ == "__main__":
     m1 = comm.bcast(m1, root=0)
     m2 = comm.bcast(m2, root=0)
 
-    # result = main_strassen(m1, m2)
-    result = strassen(m1, m2, rank)
+    result = strassen(m1, m2)
 
-    if (rank == 0):
+    if rank == 0:
         print(f"Output: {result}")
-
-
