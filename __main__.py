6e5dfcea1309d61850fb2e73f6261fb34b6a0ea2
Johnaif Abdul <johnaif.abdul@g.msuiit.edu.ph>
2024-05-10 01:13:45 +0800

    THis should be last

--- __main__.py
+++ __main__.py
@@ -19,26 +19,22 @@ if __name__ == "__main__":
     size = comm.Get_size()
 
     matrix_size = 16
-    m1 = [[] for _ in range(4)]
-    m2 = [[] for _ in range(4)]
+    comp = []
 
-    if rank < 4:
-        m1[rank] = populate_matrices(matrix_size, rank)
-    else:
-        m2[rank-4] = populate_matrices(matrix_size, rank)
+    comp = populate_matrices(matrix_size, rank)
 
     comm.Barrier()  # Synchronize all processes
 
     # Gather matrices to process 0
-    all_m1 = comm.gather(m1, root=0)
-    all_m2 = comm.gather(m2, root=0)
+    all_matrices = comm.gather(comp, root=0)
 
     # Print the matrices
     if rank == 0:
-        for i in range(2):
-            print("M1:", all_m1[i])
-            print("M2:", all_m2[i])
+        print(f"M1.1: [ {all_matrices[0]} , {all_matrices[1]} ")
         print()
-        for i in range(2, 4):
-            print("M1:", all_m1[i])
-            print("M2:", all_m2[i])
+        print(f"M1.2: [ {all_matrices[2]} , {all_matrices[3]} ")
+        print()
+        print()
+        print(f"M2.1: [ {all_matrices[4]} , {all_matrices[5]} ")
+        print()
+        print(f"M2.2: [ {all_matrices[6]} , {all_matrices[7]} ")
