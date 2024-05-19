2998775043f7860afaf2a323c85f618abb199545
Johnaif Abdul <johnaif.abdul@g.msuiit.edu.ph>
2024-05-19 15:32:51 +0800

    Reworked the whole thing so the focus is more on efficient use of space

--- __main__.py
+++ __main__.py
@@ -1,67 +1,75 @@
 from mpi4py import MPI
 import numpy as np
-from numpy.lib import source
-from numpy.matrixlib import matrix
 import time
 
 def populate_matrices(matrix_size, rank):
     size = matrix_size // 2
-    
-    # Populate matrix A
+    # Populate matrix A or B depending on the rank
     mat = np.random.randint(0, 11, size=(size, size))
-
     print(f"This is process {rank}, my mat is: {mat}")
-
     return mat
 
-def c_submatrix_solver(c_m, a_1, b_1, a_2, b_2, temp):
-    # Multiply
-    divide_n_conquer(c_m, a_1, b_1)
-    divide_n_conquer(temp, a_2, b_2)
+def multi(a, b):
+    # Perform matrix multiplication using np.matmul and return directly
+    return np.matmul(a, b)
 
-    # Add
-    for x in range(a_1.shape[0]):
-        for y in range(a_1.shape[0]):
-            c_m[x,y] += temp[x,y]
+def startitall(localmat, mat_size, rank, comm):
+    loc_size = mat_size // 2
+    temp = np.zeros((loc_size, loc_size), dtype=int)
+    # Mapping of processes to submatrices
+    # A11, A12, A21, A22, B11, B12, B21, B22
+    submat_mapping = {
+        0: "A11", 1: "A12", 2: "A21", 3: "A22",
+        4: "B11", 5: "B12", 6: "B21", 7: "B22"
+    }
 
-def naive(c_mat, a_mat, b_mat):
-    n = a_mat.shape[0]
-    for i in range(n):
-        for j in range(n):
-            c_mat[i][j] = 0
-            for k in range(n):
-                # if rank == 1:
-                #     print(f"i: {i}, j: {j}, k: {k}")
-                c_mat[i][j] += a_mat[i][k] * b_mat[k][j]
+    # Determine which submatrices each process needs to receive
+    send_recv_pairs = {
+        0: [4, 5],  # Process 0 needs B11 (4) and sends to (5) 
+        1: [6, 7],  # Process 1 needs B21 (6) and sends to (7) 
+        2: [5, 4],  # Process 2 needs B12 (5) and sends to (4) 
+        3: [7, 6],  # Process 3 needs B22 (7) and sends to (6) 
+        4: [2, 0],  # Process 4 needs A21 (2) and sends to (0) 
+        5: [0, 2],  # Process 5 needs A11 (0) and sends to (2) 
+        6: [3, 1],  # Process 6 needs A22 (3) and sends to (1) 
+        7: [1, 3]   # Process 7 needs A12 (1) and sends to (3) 
+    }
 
-def divide_n_conquer(c_mat, a_mat, b_mat):
-    # if rank == 1:
-    #     print(f"dnc c_mat: {c_mat}")
-    half = a_mat.shape[0]
-    if half == 2:
-        # if rank == 1:
-        #     print(f"{rank} a_mat b4 naive: {a_mat}")
-        #     print(f"{rank} b_mat b4 naive: {b_mat}")
-        #     print(f"{rank} c_mat b4 naive: {c_mat}")
-        naive(c_mat, a_mat, b_mat)
-        return
-    a11, a12, a21, a22 = mat_splitter(a_mat)
-    b11, b12, b21, b22 = mat_splitter(b_mat)
-    c11, c12, c21, c22 = mat_splitter(c_mat)
-    # if rank == 1:
-    #     print(f"dnc c11: {c11}, c12: {c12}, c21: {c21}, c22: {c22}")
+    submatrix_needed = send_recv_pairs[rank]
+    receive_from = submatrix_needed[i]  # Get the rank to receive from
+    send_to = submatrix_needed[(i + 1) % len(submatrix_needed)]  # Get the rank to send to
 
-    temp = np.zeros((a11.shape[0], a11.shape[0]))
-    c_submatrix_solver(c11, a11, b11, a12, b21, temp)
-    c_submatrix_solver(c12, a11, b12, a12, b22, temp)
-    c_submatrix_solver(c21, a21, b11, a22, b21, temp)
-    c_submatrix_solver(c22, a21, b12, a22, b22, temp)
+    #Phase 1
+    for i in range(len(submatrix_needed)):
+        comm.send(localmat, dest=send_to, tag=rank) #Send processes' local matrix to where it needs to go
+        localmat = comm.recv(source=receive_from, tag=receive_from) #Get matrix needed for multiplication
+    comm.Barrier()
+    
+    temp = multi(localmat, temp) #Multiply matrices and save result in temp
+    comm.Barrier()
+    
+    #Phase 2
+    # Determine which submatrices each process needs to receive
+    send_recv_pairs = {
+        0: [1],  # Process 0 needs result from (1) 
+        5: [7],  # Process 5 needs result from (7)  
+        4: [6],  # Process 4 needs result from (6)  
+        2: [3],  # Process 2 needs result from (3) 
+    }
 
-def mat_splitter(mat):
-    row = mat.shape[0]
-    half = row//2
-    return mat[:half, :half], mat[:half, half:], mat[half:, :half], mat[half:, half:]
+    if rank < 4:
+        if rank % 2 == 0:   #Processes 0 and 2 receive from 1 and 3 respectively
+            localmat = comm.recv(source=(rank+1), tag=rank)
+        else:
+            comm.send(temp, dest=(rank-1), tag=(rank-1))
+    elif rank >= 4:
+        if rank == 4 or 5: #Processes 4 and 5 receive from 6 and 7 respectively
+            localmat = comm.recv(source=(rank+2), tag=rank)
+        else:
+            comm.send(temp, dest=(rank-2), tag=(rank-2))
 
+    if rank in send_recv_pairs:
+        temp = temp + localmat  #This is where addition comes into play, this obviously don't work so....fix it
 
 # Example usage:
 if __name__ == "__main__":
@@ -69,69 +77,33 @@ if __name__ == "__main__":
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    # matrix_size = 16 # 0.00397801399230957
-    # matrix_size = 32 # 0.02890491485595703
-    # matrix_size = 64 # 0.17362570762634277
-    # matrix_size = 128 # 1.2322485446929932
-    # matrix_size = 256 # 10.044433355331421
-    # matrix_size = 512 # 89.28690814971924
-    # matrix_size = 1024 # 700.2976865768433
-    # matrix_size = 2048 # ???
-    matrix_size = 256
-    print(f"matrix_size: {matrix_size}")
-    comp = []
+    matrix_size = 8
+    localmat = []
 
     tb = 0
     if rank == 0:
         tb = time.time()
-    comp = populate_matrices(matrix_size, rank)
+    localmat = populate_matrices(matrix_size, rank)
 
     comm.Barrier()  # Synchronize all processes
 
-    # Gather matrices to process 0
-    all_matrices = comm.gather(comp, root=0)
     if rank == 0:
         ta = time.time()
-        print(f"time for number generation: {ta-tb}")
+        print(f"time for submatrix generation: {ta-tb}")
 
     # Print the matrices
-    if rank == 0:
-        print("BEFORE !!!!!!!!!!!!!!!!!!")
-        print(f"M1.1: [ {all_matrices[0]} , {all_matrices[1]} ")
-        print()
-        print(f"M1.2: [ {all_matrices[2]} , {all_matrices[3]} ")
-        print()
-        print()
-        print(f"M2.1: [ {all_matrices[4]} , {all_matrices[5]} ")
-        print()
-        print(f"M2.2: [ {all_matrices[6]} , {all_matrices[7]} ")
-        # a11, a12, a21, a22 = all_matrices[0]
+    for i in range(4):
+        if rank == i and rank % 2 == 0:
+            print(f"\nM1: {localmat}")
+        elif rank == i:
+            print(f" {localmat}")
+    
+    for i in range(4, 8):
+        if rank == i and rank % 2 == 0:
+            print(f"\nM2: {localmat}")
+        elif rank == i:
+            print(f" {localmat}")
 
-        # return mat[:half, :half], mat[:half, half:], mat[half:, :half], mat[half:, half:]
-        t1 = time.time()
-        half = all_matrices[0].shape[0]
-        comm.send([all_matrices[0], all_matrices[4], all_matrices[1], all_matrices[6]], 1, 1)
-        comm.send([all_matrices[0], all_matrices[5], all_matrices[1], all_matrices[7]], 2, 2)
-        comm.send([all_matrices[2], all_matrices[4], all_matrices[3], all_matrices[6]], 3, 3)
-        comm.send([all_matrices[2], all_matrices[5], all_matrices[3], all_matrices[7]], 4, 4)
-        res1 = comm.recv(source=1,tag=6)
-        res2 = comm.recv(source=2,tag=7)
-        res3 = comm.recv(source=3,tag=8)
-        res4 = comm.recv(source=4,tag=9)
-        t2 = time.time()
+    comm.Barrier()
 
-        print(f"time elapsed: {t2-t1}")
-
-        print("AFTER !!!!!!!!!!!!!!!!!!")
-        print(f"M1.1: [ {res1} , {res2} ")
-        print()
-        print(f"M1.2: [ {res3} , {res4} ")
-    elif rank >= 1 and rank <= 4:
-        workloads = comm.recv(source=0,tag=rank)
-        # matrix_size = matrix_size // 2
-        temp = np.zeros([workloads[1].shape[0], workloads[1].shape[0]]) #[[0]*workloads[0].shape[0]]*workloads[0].shape[0] 
-        c_sub = np.zeros([workloads[1].shape[0], workloads[1].shape[0]])
-        # print(f"temp: {temp}")
-        c_submatrix_solver(c_sub, workloads[0], workloads[1], workloads[2], workloads[3], temp)
-        print(f"temp outside: {c_sub}")
-        comm.send(c_sub,0,rank+5)
+    startitall(localmat, matrix_size, rank, comm)
